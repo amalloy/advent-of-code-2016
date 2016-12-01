2bedd86f7deb91b95db8d7424ba355095f4e4bba
Alan Malloy <alan@malloys.org>
2016-12-01 12:57:18 -0800

    Day 1 - Python style improvements suggested by @ldevesine

--- 1/src.py
+++ 1/src.py
@@ -1,6 +1,7 @@
 import sys
 
 dirs = [(1,0), (0, 1), (-1, 0), (0, -1)]
+turns = {'L': 1, 'R': -1}
 
 class Position:
     def __init__(self):
@@ -8,44 +9,38 @@ class Position:
         self.location = (0, 0)
 
     def turn(self, turn):
-        if turn == 'R':
-            delta = 1
-        elif turn == 'L':
-            delta = -1
-        else:
-            raise Exception("invalid direction '%s'" % turn)
-        self.heading = (self.heading + delta) % 4
+        self.heading = (self.heading + turns[turn]) % 4
 
     def walk(self, distance):
         (x, y) = dirs[self.heading]
         (a, b) = self.location
         self.location = (a + x * distance, b + y * distance)
 
-    def applyCommand(self, (dir, dist)):
+    def apply_command(self, (dir, dist)):
         self.turn(dir)
         self.walk(dist)
 
-def parseCommand(s):
+def parse_command(s):
     return (s[0], int(s[1:]))
 
-def parseLine(s):
-    return (parseCommand(x) for x in s.split(", "))
+def parse_line(s):
+    return (parse_command(x) for x in s.split(", "))
 
-def manhattanDistance((x, y)):
+def manhattan_distance((x, y)):
     return abs(x) + abs(y)
 
 if __name__ == '__main__':
     for line in sys.stdin:
         pos = Position()
-        commands = list(parseLine(line))
         part2 = None
         visited = set()
-        for (dir, dist) in commands:
+        for (dir, dist) in parse_line(line):
             pos.turn(dir)
             for i in xrange(dist): # so we can record intermediate locations
                 pos.walk(1)
-                if part2 is None and pos.location in visited:
-                    part2 = pos.location
-                else:
-                    visited.add(pos.location)
-        print manhattanDistance(pos.location), manhattanDistance(part2)
+                if not part2:
+                    if pos.location in visited:
+                        part2 = pos.location
+                    else:
+                        visited.add(pos.location)
+        print manhattan_distance(pos.location), manhattan_distance(part2)
