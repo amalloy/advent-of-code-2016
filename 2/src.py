79fa252ee75a97de97cc2416c040614567609b26
Alan Malloy <alan@malloys.org>
2016-12-02 13:31:11 -0800

    Remove separate 'valid' map
    Instead, treat 0 entry in the map as invalid.

--- 2/src.py
+++ 2/src.py
@@ -1,15 +1,22 @@
 import sys
 
-keypads = ([[1,2,3],[4,5,6],[7,8,9]],
-           [[0,0,1,0,0],[0,2,3,4,0],[5,6,7,8,9],[0,'A','B','C',0],[0,0,'D',0,0]])
-deltas = {'U': (-1, 0), 'D': (1, 0), 'R': (0, 1), 'L': (0, -1)}
-valids = ({0: {0,1,2}, 1: {0,1,2}, 2: {0,1,2}},
-          {0: {2}, 1: {1,2,3}, 2: set(range(5)), 3: {1,2,3}, 4: {2}})
+keypads = ([[1,2,3],
+            [4,5,6],
+            [7,8,9]],
+           [[0,0,1,0,0],
+            [0,2,3,4,0],
+            [5,6,7,8,9],
+            [0,'A','B','C',0],
+            [0,0,'D',0,0]])
 
-def move((y, x), dir, valid):
+deltas = {'U': (-1, 0), 'D': (1, 0), 'R': (0, 1), 'L': (0, -1)}
+
+def move((y, x), dir, keypad):
     (dy, dx) = deltas[dir]
     ret = (y + dy, x + dx)
-    if ret[1] in valid.get(ret[0], {}):
+    if (0 <= ret[0] < len(keypad)
+        and 0 <= ret[1] < len(keypad)
+        and keypad[ret[0]][ret[1]] != 0):
         return ret
     return (y, x)
 
@@ -17,5 +24,5 @@ if __name__ == '__main__':
     locs = [(1, 1), (2, 0)]
     for line in sys.stdin:
         for dir in line.rstrip():
-            locs = [move(loc, dir, valids[i]) for i, loc in enumerate(locs)]
+            locs = [move(loc, dir, keypads[i]) for i, loc in enumerate(locs)]
         print [keypads[i][loc[0]][loc[1]] for i, loc in enumerate(locs)]
