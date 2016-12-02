8a8093c0216f631f7f7d9bd3ce0da90b65477e64
Alan Malloy <alan@malloys.org>
2016-12-01 21:18:20 -0800

    Day 2: *replace* part 1 with part 2

--- 2/src.py
+++ 2/src.py
@@ -1,25 +1,18 @@
 import sys
 
-keypad = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+keypad = [[0,0,1,0,0],[0,2,3,4,0],[5,6,7,8,9],[0,'A','B','C',0],[0,0,'D',0,0]]
 deltas = {'U': (-1, 0), 'D': (1, 0), 'R': (0, 1), 'L': (0, -1)}
-
-def clamp((y, x)):
-    if (y < 0):
-        y = 0
-    if (y > 2):
-        y = 2
-    if (x < 0):
-        x = 0
-    if (x > 2):
-        x = 2
-    return (y, x)
+valid = {0: {2}, 1: {1,2,3}, 2: set(range(5)), 3: {1,2,3}, 4: {2}}
 
 def move((y, x), dir):
     (dy, dx) = deltas[dir]
-    return clamp((y + dy, x + dx))
+    ret = (y + dy, x + dx)
+    if ret[1] in valid.get(ret[0], {}):
+        return ret
+    return (y, x)
 
 if __name__ == '__main__':
-    pos = (1, 1)
+    pos = (2, 0)
     for line in sys.stdin:
         for dir in line.rstrip():
             pos = move(pos, dir)
