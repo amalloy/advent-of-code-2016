fae94f0e68e608d6ddb74a092c46c3b3872356f7
Alan Malloy <alan@malloys.org>
2016-12-02 21:42:35 -0800

    Day 3 - replace part 1 with part 2

--- 3/src.py
+++ 3/src.py
@@ -1,10 +1,23 @@
 import sys
+from itertools import chain
+
+def chunks(n, input):
+    for i in range(0, len(input), n):
+        yield input[i:i + n]
+
+def transpose(xs):
+    return map(list, zip(*xs))
+
+def parse(line):
+    return map(int, line.split())
 
 if __name__ == '__main__':
     valid = 0
-    for line in sys.stdin:
-        [a,b,c] = map(int, line.split())
-        print [a,b,c]
+    lines = list(sys.stdin)
+    parsed = map(parse, lines)
+    chunked = chunks(3, parsed)
+    flipped = map(transpose, chunked)
+    for [a,b,c] in chain(*flipped):
         if (a + b > c and a+c > b and b+c > a):
             valid = valid + 1
     print valid
