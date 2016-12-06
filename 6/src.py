7a491434d27d3a98a2ccbe0a5b8497c7769c6f94
Alan Malloy <alan@malloys.org>
2016-12-05 21:10:49 -0800

    Day 6, part 1

---
+++ 6/src.py
@@ -0,0 +1,9 @@
+import sys
+
+if __name__ == '__main__':
+    freqs = [{} for _ in range(8)]
+    for line in sys.stdin:
+        for i, c in enumerate(line.rstrip()):
+            freqs[i][c] = freqs[i].get(c, 0) + 1
+
+    print "".join([max(m.items(), key=lambda x: x[1])[0] for m in freqs])
