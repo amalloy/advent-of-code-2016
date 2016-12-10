7a2b1216fe48ecdc7c5e24d724b7468e88f98953
Alan Malloy <alan@malloys.org>
2016-12-10 00:50:05 -0800

    Day 9: replace part 1 with part 2

--- 9/src.py
+++ 9/src.py
@@ -5,19 +5,21 @@ marker_regex = re.compile(r'\((\d+)x(\d+)\)')
 
 def decompress(s):
     i = 0
+    ret = 0
     while i < len(s):
         m = marker_regex.match(s, i)
         if m:
             start = m.end(0)
             end = int(m.group(1)) + start
             to_repeat = s[start:end]
-            for _ in range(int(m.group(2))):
-                yield to_repeat
+            ret = ret + decompress(to_repeat) * int(m.group(2))
             i = end
         else:
-            yield s[i]
             i = i + 1
+            ret = ret + 1
+
+    return ret
 
 if __name__ == '__main__':
     for line in sys.stdin:
-        print sum(len(s) for s in decompress(line.rstrip()))
+        print decompress(line.rstrip())
