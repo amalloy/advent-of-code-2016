224a64f70a55b9e1b48a95ccde4a2e80715f8c70
Alan Malloy <alan@malloys.org>
2016-12-10 00:05:44 -0800

    Day 8, part 1

--- 8/src.py
+++ 8/src.py
@@ -6,15 +6,38 @@ class Rect:
         self.cols = cols
         self.rows = rows
 
+    def transform(self, _g):
+        return [(y, x, '#') for y in range(self.rows) for x in range(self.cols)]
+
 class Row:
     def __init__(self, row, amt):
         self.row = row
         self.amt = amt
 
+    def transform(self, g):
+        return [(self.row, (x + self.amt) % g.width, g.grid[self.row][x]) for x in range(g.width)]
+
 class Col:
     def __init__(self, col, amt):
         self.col = col
         self.amt = amt
+
+    def transform(self, g):
+        return [((y + self.amt) % g.height, self.col, g.grid[y][self.col]) for y in range(g.height)]
+
+class Grid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.grid = [['.'] * width for _ in range(height)]
+
+    def render(self):
+        for line in self.grid:
+            print "".join(line)
+
+    def apply(self, transformation):
+        for (y, x, c) in transformation:
+            self.grid[y][x] = c
 
 instr_regex = regex.compile(r"""(?|(rect)\ (\d+)x(\d+) # use same group numbers in each | branch
                                   | rotate\ (row)\ y=(\d+)\ by\ (\d+)
@@ -24,10 +47,11 @@ instr_regex = regex.compile(r"""(?|(rect)\ (\d+)x(\d+) # use same group numbers 
 parse_table = {"rect": Rect, "row": Row, "column": Col}
 def parse(line):
     match = regex.match(instr_regex, line).groups()
-    print match
     return parse_table[match[0]](*map(int, match[1:]))
 
-if __name__ == 'main':
+if __name__ == '__main__':
+    g = Grid(50 ,6)
     for line in sys.stdin:
-        print parse(line.rstrip())
-    print "done"
+        g.apply(parse(line.rstrip()).transform(g))
+
+    print sum(1 for y in range(g.height) for x in range(g.width) if g.grid[y][x] == '#')
