1c3e58669ea5143180731852dacce47643a4d1fd
Alan Malloy <alan@malloys.org>
2016-12-16 14:45:44 -0800

    Implement the key-stretching hash

--- 14/src.py
+++ 14/src.py
@@ -8,6 +8,14 @@ puzzle_input = 'yjdafjpo'
 def basic_hash(n):
   return md5(puzzle_input + str(n)).hexdigest()
 
+def stretch_hash(cycles):
+  def cycle(n):
+    ret = basic_hash(n)
+    for i in xrange(cycles):
+      ret = md5(ret).hexdigest()
+    return ret
+  return cycle
+
 def otp_keys(horizon, hash_func):
   lookahead = {k: -1 for k in '0123456789abcdef'}
   def update_lookahead(n):
@@ -23,6 +31,9 @@ def otp_keys(horizon, hash_func):
       if lookahead[triple.group(1)] > i:
         yield i
 
+def solve(hash_func):
+  print itertools.islice(otp_keys(1000, hash_func), 63, 64).next()
+
 if __name__ == '__main__':
-  part1 = otp_keys(1000, basic_hash)
-  print itertools.islice(part1, 63, 64).next()
+  solve(basic_hash)
+  solve(stretch_hash(2016))
