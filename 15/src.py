c85e9502fafb2158272a51ad96db13965deb97aa
Alan Malloy <alan@malloys.org>
2016-12-17 13:59:35 -0800

    Day 15, part 2

--- 15/src.py
+++ 15/src.py
@@ -15,7 +15,14 @@ def parse(line):
 def prune(times, (prime, residue)):
     return itertools.ifilter(lambda t: t % prime == residue, times)
 
-if __name__ == '__main__':
-    constraints = sorted([parse(line.rstrip()) for line in sys.stdin], reverse=True)
+def solve(constraints):
+    constraints = sorted(constraints, reverse=True)
     times = reduce(prune, constraints, itertools.count())
-    print times.next()
+    return times.next()
+
+if __name__ == '__main__':
+    constraints = [parse(line.rstrip()) for line in sys.stdin]
+    print "part 1: %d" % solve(constraints)
+
+    print "part 2: %d" % solve(constraints +
+            [parse("Disc #7 has 11 positions; at time=0, it is at position 0.")])
