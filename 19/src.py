09b2860963297e32fb5d60397c78fab5fc56ecf7
Alan Malloy <alan@malloys.org>
2016-12-19 00:48:22 -0800

    Day 19, part 2
    Implemented with a circular linked list.

--- 19/src.py
+++ 19/src.py
@@ -19,5 +19,27 @@ def solve(n):
         q.put(winner)
 
 
+def circle(n):
+  first = {'num': 1}
+  last = first
+  for i in xrange(1, n):
+    new = {'num': i + 1}
+    last['next'] = new
+    last = new
+    if i == (n / 2) - 1:
+      before_mid = new
+  last['next'] = first
+  return (n, before_mid)
+
+def solve2((n, before_target)):
+  while n > 1:
+    target = before_target['next']
+    before_target['next'] = target['next'] # skip over target
+    if n % 2 == 1:
+      before_target = before_target['next']
+    n = n - 1
+  return before_target['num']
+
 if __name__ == '__main__':
-    print solve(puzzle_input)
+    # print solve(puzzle_input)
+    print solve2(circle(puzzle_input))
